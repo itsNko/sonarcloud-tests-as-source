02499fbdf54cf7ec32efcfe5122db5008861f61a
Eneko Pizarro <epizarro@lksnext.com>
2024-04-24 14:17:54 +0200

    st

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
-    public static int number, a, b, c, d, e;
+    public static int number, a, b, c, d, e, f;
     int number2;
     public String potato;
     public List<Integer> list;
